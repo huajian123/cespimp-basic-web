88bec61f3e20ba6c1d2173e0c28fe391399624d3
Administrator <2095669882@qq.com>
2019-11-08 10:44:29 +0800

    菜单目录文字修改

--- src/environments/menu.ts
+++ src/environments/menu.ts
@@ -13,20 +13,119 @@ export const menus = [
             'link': '/basic-info/basic-info',
           },
           {
-            'text': '分析页',
-            'link': '/dashboard/analysis',
+            'text': '重大危险源',
+            'link': '/basic-info/major-hazard',
           },
           {
-            'text': '监控页',
-            'link': '/dashboard/monitor',
+            'text': '阈值初始化',
+            'link': '/basic-info/threshold-init',
           },
           {
-            'text': '工作台',
-            'link': '/dashboard/workplace',
+            'text': '高危工艺初始化',
+            'link': '/basic-info/high-risk-init',
+          },
+          {
+            'text': '监测数据',
+            'link': '/basic-info/monitoring-data',
           },
         ],
       },
     ],
   },
-
+  {
+    text: '危险源监控',
+    group: false,
+    hideInBreadcrumb: false,
+    children: [
+      {
+        'text': '危险源监控',
+        'icon': 'anticon-appstore',
+        'children': [
+          {
+            'text': '风险承诺',
+            'link': '/hazard-source-monitor/risk-commitment',
+          },
+          {
+            'text': '摄像头管理',
+            'link': '/hazard-source-monitor/camera-manage',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: '人员车辆监控',
+    group: false,
+    hideInBreadcrumb: false,
+    children: [
+      {
+        'text': '人员车辆监控',
+        'icon': 'anticon-appstore',
+        'children': [
+          {
+            'text': '基础信息管理',
+            'link': '/people-car-monitor/base-info-manage',
+          },
+          {
+            'text': '实名制进出管理',
+            'link': '/people-car-monitor/real-name-in-out',
+          },
+          {
+            'text': '报警区域管理',
+            'link': '/people-car-monitor/alarm-area-manage',
+          },
+          {
+            'text': '应急管理',
+            'link': '/people-car-monitor/emergency-manage',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: '区域风险监控',
+    group: false,
+    hideInBreadcrumb: false,
+    children: [
+      {
+        'text': '区域风险监控',
+        'icon': 'anticon-appstore',
+        'children': [
+          {
+            'text': '实时风险',
+            'link': '/risk-monitoring/real-time-risk',
+          },
+          {
+            'text': '风险区域管理',
+            'link': '/risk-monitoring/risk-monitoring-manage',
+          },
+          {
+            'text': '两单三卡信息管理',
+            'link': '/risk-monitoring/cards-info-manage',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: '全流程监控',
+    group: false,
+    hideInBreadcrumb: false,
+    children: [
+      {
+        'text': '全流程监控',
+        'icon': 'anticon-appstore',
+        'children': [
+          {
+            'text': '基础数据管理',
+            'link': '/whole-process-monitor/basic-info-manage',
+          },
+          {
+            'text': '告警信息管理',
+            'link': '/whole-process-monitor/alarm-info-manage',
+          },
+        ],
+      },
+    ],
+  },
 ];
