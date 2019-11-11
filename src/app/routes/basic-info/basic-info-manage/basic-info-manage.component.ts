955a3c42a616a92506c0252ed69da15edb6dfd18
huajian <287643967@qq.com>
2019-11-11 14:15:36 +0800

    修改list

--- src/app/routes/basic-info/basic-info-manage/basic-info-manage.component.ts
+++ src/app/routes/basic-info/basic-info-manage/basic-info-manage.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PageTypeEnum } from '@core/vo/comm/BusinessEnum';
-import { UfastTableNs } from '@shared/ufast-table/ufast-table.component';
 import { BasicInfoService } from '@core/biz-services/basic-info/basic-info.service';
 import { GoBackParam } from '@core/vo/comm/ReturnBackVo';
+import { STColumn } from '@delon/abc';
 
 export interface TankModel {
   id?: number;
@@ -24,6 +24,13 @@ export interface TankModel {
   fireLevel: string;
 }
 
+export interface PageInfo {
+  total: number;
+  ps: number; // 当前页码
+  pi: number; // 每页数量
+}
+
+
 @Component({
   selector: 'app-basic-info-manage',
   templateUrl: './basic-info-manage.component.html',
@@ -33,13 +40,20 @@ export class BasicInfoManageComponent implements OnInit {
   pageTypeEnum = PageTypeEnum;
   currentPage: number;
   expandForm: boolean;
-  tableConfig: UfastTableNs.TableConfig;
-  @ViewChild('operationTpl', { static: true }) operationTpl: TemplateRef<any>;
   dataList: TankModel[];
   itemId: number;
+  columns: STColumn[];
+  listPageInfo: PageInfo;
+
   constructor(private dataService: BasicInfoService) {
     this.expandForm = false;
     this.currentPage = this.pageTypeEnum.List;
+    this.columns = [];
+    this.listPageInfo = {
+      total: 0,
+      pi: 10,
+      ps: 1,
+    };
   }
 
   goDetail() {
@@ -48,131 +62,64 @@ export class BasicInfoManageComponent implements OnInit {
 
 
   private initTable(): void {
-    this.tableConfig = {
-      pageSize: 10,
-      pageNum: 1,
-      showCheckbox: false,
-      checkRowField: '_checked',
-      showPagination: true,
-      checkAll: false,
-      total: 31,
-      loading: false,
-      headers: [
-
-        {
-          title: '储罐区编号',
-          width: 140,
-          field: 'areaNo',
-          fixed: true,
-        },
-        {
-          title: '储罐区名称',
-          field: 'areaName',
-          width: 100,
-        },
-        {
-          title: '储罐区面积',
-          field: 'areaSize',
-          width: 100,
-        },
-        {
-          title: '危化品名称',
-          field: 'dangerousChemicals',
-          width: 100,
-        },
-        {
-          title: '危化品数量',
-          field: 'dangerousNum',
-          width: 100,
-        },
-        {
-          title: '地理坐标',
-          field: 'geographicInfo',
-          width: 100,
-        },
-        {
-          title: '防护堤长宽高',
-          field: 'embankmentSize',
-          width: 120,
-        },
-        {
-          title: '储罐个数',
-          field: 'tankNum',
-          width: 100,
-        },
-        {
-          title: '罐间最小距离（cm）',
-          field: 'tankDistance',
-          width: 160,
-        },
-        {
-          title: '储罐序号',
-          field: 'tankNo',
-          width: 100,
-        },
-        {
-          title: '储罐名称',
-          field: 'tankName',
-          width: 100,
-        },
-        {
-          title: '储罐形状',
-          field: 'tankShape',
-          width: 100,
-        },
-        {
-          title: '储罐形式',
-          field: 'tankType',
-          width: 100,
-          pipe: 'tankType',
-        },
-        {
-          title: '储罐材质',
-          field: 'tankMaterial',
-          width: 100,
-        },
-        {
-          title: '设计压力（kPa）',
-          field: 'designedPressure',
-          width: 150,
-        },
-        {
-          title: '火灾危险性等级',
-          field: 'fireLevel',
-          pipe: 'fireLevelType',
-          width: 120,
-        },
-        {
-          fixed: true,
-          title: '操作',
-          tdTemplate: this.operationTpl,
-          width: 150,
-        },
-      ],
-    };
+    this.columns = [
+      { title: '储罐区编号', index: 'areaNo', width: 140 },
+      { title: '储罐区名称', index: 'areaName', width: 140 },
+      { title: '储罐区面积', index: 'areaSize', width: 140 },
+      { title: '危化品名称', index: 'dangerousChemicals', width: 140 },
+      { title: '危化品数量', index: 'dangerousNum', width: 140 },
+      { title: '地理坐标', index: 'geographicInfo', width: 140 },
+      { title: '防护堤长宽高', index: 'embankmentSize', width: 140 },
+      { title: '储罐个数', index: 'tankNum', width: 140 },
+      { title: '罐间最小距离（cm）', index: 'tankDistance', width: 180 },
+      { title: '储罐序号', index: 'tankNo', width: 140 },
+      { title: '储罐名称', index: 'tankName', width: 140 },
+      { title: '储罐形状', index: 'tankShape', width: 140 },
+      { title: '储罐形式', index: 'tankType', width: 140 },
+      { title: '储罐材质', index: 'tankMaterial', width: 140 },
+      { title: '设计压力（kPa）', index: 'designedPressure', width: 140 },
+      { title: '火灾危险性等级', index: 'fireLevel', width: 140 },
+      {
+        title: '操作',
+        fixed: 'right',
+        width: '180px',
+        buttons: [
+          {
+            text: 'Edit',
+            icon: 'edit',
+            click: (_record, modal) => console.log(_record,modal),
+          },
+          {
+            text: 'Edit',
+            icon: 'edit',
+            click: (_record, modal) => 123,
+          },
+        ],
+      },
+    ];
   }
+
   async returnToList(e?: GoBackParam) {
     this.currentPage = this.pageTypeEnum.List;
     if (!!e && e.refesh) {
       await this.getDataList(e.pageNo);
     }
   }
+
   async getDataList(pageNumber?: number) {
     const params = {
-      pageNum: pageNumber || this.tableConfig.pageNum,
-      pageSize: this.tableConfig.pageSize,
+      pageNum: 1,
+      pageSize: 10,
       filters: {},
     };
     await this.dataService.getFactoryList(params);
     const { total, list, pageNum } = await this.dataService.getFactoryList(params);
-    this.tableConfig.total = total;
-    this.tableConfig.pageNum = pageNum;
+    this.listPageInfo.total = total;
+    this.listPageInfo.ps = pageNum;
     this.dataList = list || [];
   }
-
   ngOnInit() {
     this.initTable();
     this.getDataList();
   }
-
 }
