fb3a85e4d48c312d8336bb543020e30a37ddd494
huajian <287643967@qq.com>
2019-11-12 18:05:28 +0800

    生产场所列表展示

---
+++ src/app/core/biz-services/major-hazard-management/production-list.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpUtilService } from '@core/net/http-util.service';
+import { SearchCommonVO } from '@core/vo/comm/BusinessEnum';
+import { PageInfo } from '@core/vo/comm/PageInfo';
+export namespace ProductionListServiceNs {
+  export interface ProductionListInfoModel {
+    id: number;
+    placeNo: string;
+    roomName: string;
+    placeArea?: number;
+    productionDate?: Date;
+    longitude: number; // 经度
+    latitude: number; // 纬度
+    locFactory?: string;
+  }
+
+  export interface EntprSearch {
+    entprId: number;
+  }
+
+  export class ProductionListInfoServiceClass {
+    constructor(private http: HttpUtilService) {
+    }
+
+    public getProductionInfoDetail(param: EntprSearch): Promise<ProductionListInfoModel> {
+      return this.http.get('data/basic/enterprise/'+param.entprId).toPromise();
+    }
+
+    public getProductionList(param: SearchCommonVO): Promise<PageInfo<ProductionListInfoModel>> {
+      return this.http.get('data/basic/enterprises', param).toPromise();
+    }
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductionListInfoService extends ProductionListServiceNs.ProductionListInfoServiceClass {
+
+  constructor(http: HttpUtilService) {
+    super(http);
+  }
+
+}
--- src/app/routes/major-hazard-management/production-list/production-list.component.ts
+++ src/app/routes/major-hazard-management/production-list/production-list.component.ts
@@ -1,45 +1,115 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { _HttpClient, ModalHelper } from '@delon/theme';
-import { STColumn, STComponent } from '@delon/abc';
-import { SFSchema } from '@delon/form';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { STColumn } from '@delon/abc';
+import { ListPageInfo, PageTypeEnum } from '@core/vo/comm/BusinessEnum';
+import {
+  ProductionListInfoService,
+  ProductionListServiceNs,
+} from '@core/biz-services/major-hazard-management/production-list.service';
+import ProductionListInfoModel = ProductionListServiceNs.ProductionListInfoModel;
+import { MapPipe } from '@shared/directives/pipe/map.pipe';
+import { GoBackParam } from '@core/vo/comm/ReturnBackVo';
 
 @Component({
   selector: 'app-major-hazard-management-production-list',
   templateUrl: './production-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MajorHazardManagementProductionListComponent implements OnInit {
-  url = `/user`;
-  searchSchema: SFSchema = {
-    properties: {
-      no: {
-        type: 'string',
-        title: '编号'
-      }
-    }
-  };
-  @ViewChild('st', { static: false }) st: STComponent;
-  columns: STColumn[] = [
-    { title: '编号', index: 'no' },
-    { title: '调用次数', type: 'number', index: 'callNo' },
-    { title: '头像', type: 'img', width: '50px', index: 'avatar' },
-    { title: '时间', type: 'date', index: 'updatedAt' },
-    {
-      title: '',
-      buttons: [
-        // { text: '查看', click: (item: any) => `/form/${item.id}` },
-        // { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
-      ]
-    }
-  ];
+  pageTypeEnum = PageTypeEnum;
+  currentPage: number;
+  expandForm: boolean;
+  dataList: ProductionListInfoModel[];
+  columns: STColumn[];
+  listPageInfo: ListPageInfo;
+  itemId: number;
 
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
-
-  ngOnInit() { }
-
-  add() {
-    // this.modal
-    //   .createStatic(FormEditComponent, { i: { id: 0 } })
-    //   .subscribe(() => this.st.reload());
+  constructor(private dataService: ProductionListInfoService, private cdr: ChangeDetectorRef) {
+    this.expandForm = false;
+    this.currentPage = this.pageTypeEnum.List;
+    this.columns = [];
+    this.listPageInfo = {
+      total: 0,
+      ps: 10,//每页数量
+      pi: 1,// 当前页码
+    };
+    this.dataList = [];
+    this.itemId = -1;
   }
 
+  changePage(e) {
+    this.listPageInfo = e;
+    this.getDataList();
+  }
+
+  async getDataList(pageNumber?: number) {
+    const params = {
+      pageNum: pageNumber || this.listPageInfo.pi,
+      pageSize: this.listPageInfo.ps,
+    };
+    const { total, list, pageNum } = await this.dataService.getProductionList(params);
+    this.listPageInfo.total = total;
+    this.listPageInfo.pi = pageNum;
+    this.dataList = list || [];
+    this.cdr.markForCheck();
+  }
+
+  format(toBeFormat, arg) {
+    return new MapPipe().transform(toBeFormat, arg);
+  }
+
+  goEditAddPage(item, modal) {
+    this.currentPage = this.pageTypeEnum.AddOrEdit;
+  }
+
+  goDetailPage(item, modal) {
+    this.itemId = item.id;
+    this.currentPage = this.pageTypeEnum.DetailOrExamine;
+  }
+
+  async returnToList(e?: GoBackParam) {
+    this.currentPage = this.pageTypeEnum.List;
+    if (!!e && e.refesh) {
+      this.listPageInfo.ps = e.pageNo;
+      await this.getDataList(e.pageNo);
+    }
+  }
+
+  private initTable(): void {
+    this.columns = [
+      { title: '生产场所编号', index: 'roomNo', width: 120 },
+      { title: '生产场所名称', index: 'roomName', width: 100 },
+      { title: '生产场所面积', index: 'roomArea', width: 120 },
+      { title: '投产时间', index: 'roomForm', width: 100 },
+      { title: '经度', index: 'longitude', width: 100 },
+      { title: '纬度', index: 'latitude', width: 100 },
+      { title: '在厂区的位置', index: 'locFactory', width: 100 },
+      {
+        title: '操作',
+        fixed: 'right',
+        width: '100px',
+        buttons: [
+          {
+            text: '编辑',
+            icon: 'edit',
+            click: this.goEditAddPage.bind(this),
+          },
+          {
+            text: '删除',
+            icon: 'delete',
+            click: (_record, modal) => 123,
+          },
+          {
+            text: '查看',
+            icon: 'eye',
+            click: this.goDetailPage.bind(this),
+          },
+        ],
+      },
+    ];
+  }
+
+  ngOnInit() {
+    this.initTable();
+    this.getDataList();
+  }
 }
