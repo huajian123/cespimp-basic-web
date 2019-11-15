c01ecd27edb29c0f14a6426fdb41f39082618f94
huajian <287643967@qq.com>
2019-11-15 16:30:20 +0800

    储罐列表和库区列表接口地址更改

--- src/app/core/biz-services/warehouse-management/warehouse-list.service.ts
+++ src/app/core/biz-services/warehouse-management/warehouse-list.service.ts
@@ -31,7 +31,7 @@ export namespace WarehouseListServiceNs {
     }
 
     public getWarehouseList(param: SearchCommonVO): Promise<PageInfo<WarehouseListInfoModel>> {
-      return this.http.get('data/major/hazard/rooms', param).toPromise();
+      return this.http.get('data/basic/enterprise/rooms', param).toPromise();
     }
   }
 }
