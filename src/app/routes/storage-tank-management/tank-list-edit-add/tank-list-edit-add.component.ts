5122bf9f6e7dcb7b7443110c9bae008624e6fbe0
huajian <287643967@qq.com>
2019-11-20 09:45:23 +0800

    修改详情

--- src/app/routes/storage-tank-management/tank-list-edit-add/tank-list-edit-add.component.ts
+++ src/app/routes/storage-tank-management/tank-list-edit-add/tank-list-edit-add.component.ts
@@ -8,7 +8,7 @@ import {
   Output,
 } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PositionPickerService } from '../../../widget/position-picker/position-picker.service';
 import { LoginInfoModel } from '@core/vo/comm/BusinessEnum';
 import { EVENT_KEY } from '@env/staticVariable';
@@ -27,110 +27,153 @@ interface OptionsInterface {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StorageTankManagementTankListEditAddComponent implements OnInit {
-  validateForm: FormGroup;
-  form: FormGroup;
   @Input() id: number;
   @Input() currentPageNum: number;
   @Output() returnBack: EventEmitter<any>;
-  loginInfo: LoginInfoModel;
-  tankFormOptions: OptionsInterface[];
-  tankStructureOptions: OptionsInterface[];
-  tankMateOptions: OptionsInterface[];
-  tankTypeOptions: OptionsInterface[];
+  editIndex = -1;
+  editObj = {};
 
+  form: FormGroup;
+  users: any[] = [{ value: 'xiao', label: '付晓晓' }, { value: 'mao', label: '周毛毛' }];
 
-  constructor(private fb: FormBuilder, private msg: NzMessageService, private cdr: ChangeDetectorRef,
-              private dataService: TankListInfoService, private positionPickerService: PositionPickerService) {
+  constructor(private fb: FormBuilder) {
     this.returnBack = new EventEmitter<any>();
-    this.tankFormOptions = [];
-    this.tankStructureOptions = [];
-    this.tankMateOptions = [];
-    this.tankTypeOptions = [];
-
-    this.loginInfo = {
-      createBy: '',
-      createTime: new Date(),
-      delFlag: null,
-      entprId: null,
-      id: null,
-      mobileTel: '',
-      password: '',
-      realName: '',
-      role: null,
-      updateBy: '',
-      updateTime: new Date(),
-      userName: '',
-    };
-  }
-
-  initForm() {
-    this.validateForm = this.fb.group({
-      tankNo: [null, [Validators.required]],
-      tankName: [null, [Validators.required]],
-      tankType: [null, [Validators.required]],
-      tankForm: [null, []],
-      tankStructure: [null, []],
-      tankMate: [null, []],
-      tamkCapacity: [null, []],
-      productionDate: [null, []],
-      longitude: [null, [Validators.required]],
-      latitude: [null, [Validators.required]],
-      locFactory: [null, []],
-    });
-  }
-
-  async getDetail() {
-    const dataInfo = await this.dataService.getTankInfoDetail(this.id);
-    this.validateForm.patchValue(dataInfo);
-    this.cdr.markForCheck();
-  }
-
-  returnToList() {
-    this.returnBack.emit();
-  }
-
-  showMap() {
-    this.positionPickerService.show({ isRemoteImage: true }).then(res => {
-      this.validateForm.get('longitude').setValue(res.longitude);
-      this.validateForm.get('latitude').setValue(res.latitude);
-    }).catch(e => null);
-  }
-
-  async submit() {
-    Object.keys(this.validateForm.controls).forEach(key => {
-      this.validateForm.controls[key].markAsDirty();
-      this.validateForm.controls[key].updateValueAndValidity();
-    });
-    if (this.validateForm.invalid) {
-      return;
-    }
-    const params = this.validateForm.getRawValue();
-    params.entprId = this.loginInfo.entprId;
-    params.createBy = this.loginInfo.createBy;
-    params.updateBy = this.loginInfo.updateBy;
-    let submitHandel = null;
-
-    if (!this.id) {
-      submitHandel = this.dataService.addTank(params);
-    } else {
-      params.id = this.id;
-      submitHandel = this.dataService.editTank(params);
-    }
-
-    await submitHandel;
-    this.returnBack.emit({ refesh: true, pageNo: this.currentPageNum });
   }
 
   ngOnInit() {
-    this.loginInfo = JSON.parse(window.sessionStorage.getItem(EVENT_KEY.loginInfo));
-    this.tankTypeOptions = [...MapPipe.transformMapToArray(MapSet.tankType)];
-    this.tankFormOptions = [...MapPipe.transformMapToArray(MapSet.tankForm)];
-    this.tankStructureOptions = [...MapPipe.transformMapToArray(MapSet.tankStructure)];
-    this.tankMateOptions = [...MapPipe.transformMapToArray(MapSet.tankMate)];
-    this.initForm();
-    if (this.id) {
-      this.getDetail();
-    }
+    this.form = this.fb.group({
+      name: [null, [Validators.required]],
+      url: [null, [Validators.required]],
+      owner: [undefined, [Validators.required]],
+      approver: [null, [Validators.required]],
+      date_range: [null, [Validators.required]],
+      type: [null, [Validators.required]],
+      name2: [null, [Validators.required]],
+      summary: [null, [Validators.required]],
+      owner2: [null, [Validators.required]],
+      approver2: [null, [Validators.required]],
+      time: [null, [Validators.required]],
+      type2: [null, [Validators.required]],
+      items: this.fb.array([]),
+    });
+    const userList = [
+      {
+        key: '1',
+        workId: '00001',
+        name: 'John Brown',
+        department: 'New York No. 1 Lake Park',
+      },
+      {
+        key: '2',
+        workId: '00002',
+        name: 'Jim Green',
+        department: 'London No. 1 Lake Park',
+      },
+      {
+        key: '3',
+        workId: '00003',
+        name: 'Joe Black',
+        department: 'Sidney No. 1 Lake Park',
+      },
+    ];
+    userList.forEach(i => {
+      const field = this.createUser();
+      field.patchValue(i);
+      this.items.push(field);
+    });
   }
 
+  createUser(): FormGroup {
+    return this.fb.group({
+      key: [null],
+      workId: [null, [Validators.required]],
+      name: [null, [Validators.required]],
+      department: [null, [Validators.required]],
+    });
+  }
+
+  //#region get form fields
+  get name() {
+    return this.form.controls.name;
+  }
+  get url() {
+    return this.form.controls.url;
+  }
+  get owner() {
+    return this.form.controls.owner;
+  }
+  get approver() {
+    return this.form.controls.approver;
+  }
+  get time_start() {
+    return this.form.controls.time_start;
+  }
+  get time_end() {
+    return this.form.controls.time_end;
+  }
+  get type() {
+    return this.form.controls.type;
+  }
+  get name2() {
+    return this.form.controls.name2;
+  }
+  get summary() {
+    return this.form.controls.summary;
+  }
+  get owner2() {
+    return this.form.controls.owner2;
+  }
+  get approver2() {
+    return this.form.controls.approver2;
+  }
+  get time() {
+    return this.form.controls.time;
+  }
+  get type2() {
+    return this.form.controls.type2;
+  }
+  get items() {
+    return this.form.controls.items as FormArray;
+  }
+  //#endregion
+
+  add() {
+    this.items.push(this.createUser());
+    this.edit(this.items.length - 1);
+  }
+
+  del(index: number) {
+    this.items.removeAt(index);
+  }
+
+  edit(index: number) {
+    if (this.editIndex !== -1 && this.editObj) {
+      this.items.at(this.editIndex).patchValue(this.editObj);
+    }
+    this.editObj = { ...this.items.at(index).value };
+    this.editIndex = index;
+  }
+
+  save(index: number) {
+    this.items.at(index).markAsDirty();
+    if (this.items.at(index).invalid) return;
+    this.editIndex = -1;
+  }
+
+  cancel(index: number) {
+    if (!this.items.at(index).value.key) {
+      this.del(index);
+    } else {
+      this.items.at(index).patchValue(this.editObj);
+    }
+    this.editIndex = -1;
+  }
+
+  _submitForm() {
+    Object.keys(this.form.controls).forEach(key => {
+      this.form.controls[key].markAsDirty();
+      this.form.controls[key].updateValueAndValidity();
+    });
+    if (this.form.invalid) return;
+  }
 }
