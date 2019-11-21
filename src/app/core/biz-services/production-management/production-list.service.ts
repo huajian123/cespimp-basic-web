79ef1c51e6dcff7cc8374b2f39088307857c78fd
huajian <287643967@qq.com>
2019-11-22 05:27:26 +0800

    添加园区详情的返回按钮

--- src/app/core/biz-services/production-management/production-list.service.ts
+++ src/app/core/biz-services/production-management/production-list.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpUtilService } from '@core/net/http-util.service';
 import { SearchCommonVO } from '@core/vo/comm/BusinessEnum';
 import { PageInfo } from '@core/vo/comm/PageInfo';
+
 export namespace ProductionListServiceNs {
   export interface ProductionListInfoModel {
     id: number;
@@ -12,29 +13,34 @@ export namespace ProductionListServiceNs {
     longitude: number; // 经度
     latitude: number; // 纬度
     locFactory?: string;
+    majorHazardMaterials: any[]
   }
 
 
   export class ProductionListInfoServiceClass {
     constructor(private http: HttpUtilService) {
     }
+
     /*新增接口*/
-    public addProduction(param:ProductionListInfoModel): Promise<void> {
-      return this.http.post('data/basic/enterprise/place',param,{needSuccessInfo: true}).toPromise();
+    public addProduction(param: ProductionListInfoModel): Promise<void> {
+      return this.http.post('data/basic/enterprise/place', param, { needSuccessInfo: true }).toPromise();
     }
+
     /*修改接口*/
     public editProduction(param: ProductionListInfoModel): Promise<void> {
-      return this.http.put('data/basic/enterprise/place',param,{needSuccessInfo: true}).toPromise();
+      return this.http.put('data/basic/enterprise/place', param, { needSuccessInfo: true }).toPromise();
     }
-    public getProductionInfoDetail(id:number): Promise<ProductionListInfoModel> {
-      return this.http.get('data/basic/enterprise/place/'+id).toPromise();
+
+    public getProductionInfoDetail(id: number): Promise<ProductionListInfoModel> {
+      return this.http.get('data/basic/enterprise/place/' + id).toPromise();
     }
 
     public getProductionList(param: SearchCommonVO): Promise<PageInfo<ProductionListInfoModel>> {
       return this.http.get('data/basic/enterprise/place', param).toPromise();
     }
+
     public delProductionInfo(id: number): Promise<ProductionListInfoModel> {
-      return this.http.del('data/basic/enterprise/place/'+ id).toPromise();
+      return this.http.del('data/basic/enterprise/place/' + id).toPromise();
     }
   }
 }
--- src/app/routes/production-management/production-list-detail/production-list-detail.component.ts
+++ src/app/routes/production-management/production-list-detail/production-list-detail.component.ts
@@ -12,6 +12,11 @@ import {
   ProductionListServiceNs,
 } from '@core/biz-services/production-management/production-list.service';
 import ProductionListInfoModel = ProductionListServiceNs.ProductionListInfoModel;
+import { STColumn } from '@delon/abc';
+import { _HttpClient } from '@delon/theme';
+import { NzMessageService } from 'ng-zorro-antd';
+import { WarehouseListInfoService } from '@core/biz-services/warehouse-management/warehouse-list.service';
+import { PositionPickerService } from '../../../widget/position-picker/position-picker.service';
 
 
 @Component({
@@ -24,32 +29,56 @@ export class ProductionManagementProductionListDetailComponent implements OnInit
   @Input() id: number;
   @Input() currentPageNum: number;
   dataInfo: ProductionListInfoModel;
-  constructor(private dataService: ProductionListInfoService, private cdr: ChangeDetectorRef) {
+  columns: STColumn[];
+
+  constructor(private http: _HttpClient, private msg: NzMessageService,
+              private dataService: ProductionListInfoService, private cdr: ChangeDetectorRef,
+              private positionPickerService: PositionPickerService) {
+    this.returnBack = new EventEmitter<any>();
+
     this.dataInfo = {
-      id: null,
+      id: -1,
       placeNo: '',
       placeName: '',
-      placeArea: null,
+      placeArea: -1,
+      longitude: -1,
+      latitude: -1,
       productionDate: new Date(),
-      longitude: null,
-      latitude: null,
       locFactory: '',
+      majorHazardMaterials: [],
     };
-    this.returnBack = new EventEmitter<any>();
+
+    this.columns = [
+      { title: '品名', index: 'productName', width: 60 },
+      { title: 'CAS号', index: 'casNo', width: 60 },
+      { title: '临界量（吨）', index: 'criticalMass', width: 60 },
+      {
+        title: '设计贮存最大量（吨）',
+        index: 'maximumReserves',
+        width: 60,
+      },
+    ];
   }
 
-  async getDetailInfo(){
+  returnToList() {
+    this.returnBack.emit({ refesh: false, pageNo: this.currentPageNum });
+  }
+
+  showMap() {
+    this.positionPickerService.show({
+      isRemoteImage: true,
+      longitude: this.dataInfo.longitude,
+      latitude: this.dataInfo.latitude,
+    }).then().catch(e => e);
+  }
+
+  async getDetailInfo() {
     this.dataInfo = await this.dataService.getProductionInfoDetail(this.id);
+    console.log(this.dataInfo);
     this.cdr.markForCheck();
   }
 
-
-  returnBackToList() {
-    this.returnBack.emit({refesh: false, pageNo: this.currentPageNum});
+  ngOnInit(): void {
+    this.getDetailInfo();
   }
-
-  ngOnInit() {
-    this.getDetailInfo()
-  }
-
 }
