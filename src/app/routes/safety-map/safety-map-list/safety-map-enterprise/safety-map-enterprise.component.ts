05431eae1df9bee15210f979f5c21f117b3101fb
huajian <287643967@qq.com>
2019-11-23 17:12:40 +0800

    添加园区详情的返回按钮

--- src/app/routes/safety-map/safety-map-list/safety-map-enterprise/safety-map-enterprise.component.ts
+++ src/app/routes/safety-map/safety-map-list/safety-map-enterprise/safety-map-enterprise.component.ts
@@ -1,6 +1,22 @@
 import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { BasicInfoService } from '@core/biz-services/basic-info/basic-info.service';
 
+enum LayerEnum {
+  Temperature,
+  Pressure,
+  WaterLevel,
+  FireGas,
+  PoisonousGas,
+  Camera
+}
+
+interface LayerBtnInterface {
+  name: string;
+  type: string;
+  icon: string;
+  isSel: boolean;
+}
+
 @Component({
   selector: 'safety-map-enterprise',
   templateUrl: './safety-map-enterprise.component.html',
@@ -9,12 +25,32 @@ import { BasicInfoService } from '@core/biz-services/basic-info/basic-info.servi
 })
 export class SafetyMapEnterpriseComponent implements OnInit, AfterViewInit {
   map;
+  layerEnum = LayerEnum;
   tilePhoto: Object; // 倾斜摄影对象
+  currentSelLayerBtnIndex: number;
+  layerBtnObjArray: LayerBtnInterface[];
+
   constructor(private cdr: ChangeDetectorRef) {
     const imageURL = 'http://t0.tianditu.gov.cn/img_w/wmts?' +
       'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles' +
       '&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=0a65163e2ebdf5a37abb7f49274b85df';
     this.tilePhoto = new T.TileLayer(imageURL, { minZoom: 1, maxZoom: 18 });
+    this.currentSelLayerBtnIndex = -1;
+    this.layerBtnObjArray = [
+      { name: '温度传感器', type: 'default', icon: 'download', isSel: false },
+      { name: '压力传感器', type: 'default', icon: 'download', isSel: false },
+      { name: '液位传感器', type: 'default', icon: 'download', isSel: false },
+      { name: '可燃气体', type: 'default', icon: 'download', isSel: false },
+      { name: '有毒气体', type: 'default', icon: 'download', isSel: false },
+      { name: '摄像头', type: 'default', icon: 'download', isSel: false },
+    ];
+  }
+
+  // 切换图层
+  changeLayer(layerEnum, item) {
+    item.isSel = !item.isSel;
+    this.currentSelLayerBtnIndex = layerEnum;
+
   }
 
   initMap() {
