9ac144e205a26e05f9d63c809b172364f2791c4e
huajian <287643967@qq.com>
2019-11-23 23:02:31 +0800

    特种作业模块下的9个子菜单列表页的数据接口调用

---
+++ src/app/core/biz-services/special-operation-management/special-operation-management.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { HttpUtilService } from '@core/net/http-util.service';
+import { SearchCommonVO } from '@core/vo/comm/BusinessEnum';
+import { PageInfo } from '@core/vo/comm/PageInfo';
+
+export namespace SpecialOperationManagementServiceNs {
+  export interface SpecialOperationInfoModel {
+    id: number;
+    operationType: SpecialInfoEnum;
+    operationName: string;
+    operationPlace: string;
+    operationContent: string;
+    operationCertificate: string;
+    applicationName: string;
+    guardianName: string;
+    leadingName: string;
+    operationStartTime: Date;
+    operationEndTime: Date;
+    reviewName?: string;
+    reviewTime?: Date;
+    reviewExplain?: number;
+    reviewStatus: number;
+  }
+  export enum SpecialInfoEnum {
+    HotWork = 1, //动火作业
+    ConfinedSpaceWork, // 受限空间作业
+    ElevatedWork,// 高处作业
+    HoistingOperation,//吊装作业
+    TemporaryElectricity,//临时用电
+    EquipmentOverhaul,//设备检修
+    BlindPlate,//盲板抽堵
+    OpenCircuit,//断路作业
+    EarthMoving,//动土作业
+  }
+
+  export class SpecialOperationInfoServiceClass {
+    constructor(private http: HttpUtilService) {
+    }
+
+    /*新增接口*/
+   /* public addWarehouse(param: SpecialOperationInfoModel): Promise<void> {
+      return this.http.post('data/basic/enterprise/rooms', param, { needSuccessInfo: true }).toPromise();
+    }*/
+
+    /*修改接口*/
+  /*  public editWarehouse(param: SpecialOperationInfoModel): Promise<void> {
+      return this.http.put('data/basic/enterprise/rooms', param, { needSuccessInfo: true }).toPromise();
+    }*/
+
+    /*库房详情*/
+  /*  public getWarehouseInfoDetail(id: number): Promise<SpecialOperationInfoModel> {
+      return this.http.get('data/basic/enterprise/rooms/' + id).toPromise();
+    }*/
+
+    /*特种作业列表*/
+    public getSpecialOperationList(param: SearchCommonVO): Promise<PageInfo<SpecialOperationInfoModel>> {
+      return this.http.get('data/basic/enterprise/specials', param).toPromise();
+    }
+
+    /*删除接口*/
+  /*  public delWarehouseInfo(id: number): Promise<SpecialOperationInfoModel> {
+      return this.http.del('data/basic/enterprise/rooms/' + id).toPromise();
+    }*/
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SpecialOperationInfoService extends SpecialOperationManagementServiceNs.SpecialOperationInfoServiceClass {
+  constructor(http: HttpUtilService) {
+    super(http);
+  }
+
+}
--- src/app/routes/special-operation-management/temporary-electricity-list/temporary-electricity-list.component.ts
+++ src/app/routes/special-operation-management/temporary-electricity-list/temporary-electricity-list.component.ts
@@ -1,45 +1,139 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { _HttpClient, ModalHelper } from '@delon/theme';
-import { STColumn, STComponent } from '@delon/abc';
-import { SFSchema } from '@delon/form';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { STColumn, STComponent, STData } from '@delon/abc';
+import { ListPageInfo, PageTypeEnum, RoleEnum } from '@core/vo/comm/BusinessEnum';
+import {
+  SpecialOperationInfoService,
+  SpecialOperationManagementServiceNs,
+} from '@core/biz-services/special-operation-management/special-operation-management.service';
+import SpecialOperationInfoModel = SpecialOperationManagementServiceNs.SpecialOperationInfoModel;
+import { MessageType, ShowMessageService } from '../../../widget/show-message/show-message';
+import { MapPipe } from '@shared/directives/pipe/map.pipe';
 
 @Component({
   selector: 'app-special-operation-management-temporary-electricity-list',
   templateUrl: './temporary-electricity-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SpecialOperationManagementTemporaryElectricityListComponent implements OnInit {
-  url = `/user`;
-  searchSchema: SFSchema = {
-    properties: {
-      no: {
-        type: 'string',
-        title: '编号'
-      }
-    }
-  };
-  @ViewChild('st', { static: false }) st: STComponent;
-  columns: STColumn[] = [
-    { title: '编号', index: 'no' },
-    { title: '调用次数', type: 'number', index: 'callNo' },
-    { title: '头像', type: 'img', width: '50px', index: 'avatar' },
-    { title: '时间', type: 'date', index: 'updatedAt' },
-    {
-      title: '',
-      buttons: [
-        // { text: '查看', click: (item: any) => `/form/${item.id}` },
-        // { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
-      ]
-    }
-  ];
+  roleEnum = RoleEnum;
+  pageTypeEnum = PageTypeEnum;
+  currentPage: number;
+  expandForm: boolean;
+  dataList: SpecialOperationInfoModel[];
+  columns: STColumn[];
+  listPageInfo: ListPageInfo;
+  itemId: number;
 
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
-
-  ngOnInit() { }
-
+  constructor(private dataService: SpecialOperationInfoService, private cdr: ChangeDetectorRef, private messageService: ShowMessageService) {
+    this.expandForm = false;
+    this.currentPage = this.pageTypeEnum.List;
+    this.columns = [];
+    this.listPageInfo = {
+      total: 0,
+      ps: 10,// 每页数量
+      pi: 1,// 当前页码
+    };
+    this.dataList = [];
+    this.itemId = -1;
+  }
+  async getDataList(currentType = 5) {
+    const params = {
+      operationType: currentType,
+      pageNum:  this.listPageInfo.pi,
+      pageSize: this.listPageInfo.ps,
+    };
+    const { total, list, pageNum } = await this.dataService.getSpecialOperationList(params);
+    this.listPageInfo.total = total;
+    this.listPageInfo.pi = pageNum;
+    this.dataList = list || [];
+    this.cdr.markForCheck();
+  }
+  changePage(e) {
+    this.listPageInfo = e;
+    this.getDataList();
+  }
   add() {
-    // this.modal
-    //   .createStatic(FormEditComponent, { i: { id: 0 } })
-    //   .subscribe(() => this.st.reload());
+    this.itemId = null;
+    this.currentPage = this.pageTypeEnum.AddOrEdit;
+  }
+  format(toBeFormat, arg) {
+    return new MapPipe().transform(toBeFormat, arg);
+  }
+  private initTable(): void {
+    this.columns = [
+      { title: '企业名称', index: 'entprName', width: 120, acl: this.roleEnum[this.roleEnum.ParkManage] },
+      { title: '特种作业类型',
+        index: 'operationType',
+        width: 120,
+        format: (item: STData, _col: STColumn, index) => this.format(item[_col.indexKey], _col.indexKey)
+      },
+      { title: '作业名称', index: 'operationName', width: 100 },
+      { title: '作业地点', index: 'operationPlace', width: 120 },
+      { title: '作业内容', index: 'operationContent', width: 100 },
+      { title: '作业证附件', index: 'operationCertificate', width: 100 },
+      { title: '申请人', index: 'applicationName', width: 100 },
+      { title: '申请时间', index: 'operationStartTime', width: 100,type:'date'  },
+      { title: '监护人', index: 'operationEndTime', width: 100 },
+      { title: '负责人', index: 'leadingName', width: 100 },
+      { title: '作业开始时间', index: 'operationStartTime', width: 100,type:'date' },
+      { title: '作业结束时间', index: 'operationEndTime', width: 100,type:'date' },
+      { title: '审核人', index: 'reviewName', width: 100 },
+      { title: '审核时间', index: 'reviewTime', width: 100 ,type:'date' },
+      { title: '审核意见', index: 'reviewExplain', width: 100 },
+      { title: '审核状态',
+        index: 'reviewStatus',
+        width: 100,
+        format: (item: STData, _col: STColumn, index) => this.format(item[_col.indexKey], _col.indexKey),
+      },
+      {
+        title: '操作',
+        fixed: 'right',
+        width: '100px',
+        buttons: [
+          {
+            text: '编辑',
+            icon: 'edit',
+            click: this.goEditAddPage.bind(this),
+            acl: this.roleEnum[this.roleEnum.Enterprise],
+          },
+          {
+            text: '删除',
+            icon: 'delete',
+            click: this.goDeletePage.bind(this),
+            acl: this.roleEnum[this.roleEnum.Enterprise],
+          },
+          {
+            text: '查看',
+            icon: 'eye',
+            click: this.goDetailPage.bind(this),
+          },
+        ],
+      },
+    ];
+  }
+  goDetailPage(item, modal) {
+    this.itemId = item.id;
+    this.currentPage = this.pageTypeEnum.DetailOrExamine;
+  }
+  goEditAddPage(item, modal) {
+    this.itemId = item.id;
+    this.currentPage = this.pageTypeEnum.AddOrEdit;
+  }
+
+  goDeletePage(item, modal) {
+    const modalCtrl = this.messageService.showAlertMessage('', '您确定要删除吗？', MessageType.Confirm);
+    modalCtrl.afterClose.subscribe((type: string) => {
+      if (type !== 'onOk') {
+        return;
+      }
+      this.itemId = item.id;
+      // this.dataService.delWarehouseInfo(this.itemId).then(() => this.getDataList(1));
+    });
+  }
+
+  ngOnInit() {
+    this.initTable();
+    this.getDataList();
   }
 
 }
