269e64924775c01a6e2913d76e7dde3063c7ecf0
huajian <287643967@qq.com>
2019-12-07 15:35:45 +0800

    安全一张图添加视频直播功能

--- src/app/routes/safety-map/safety-map-list/safety-map-enterprise/camera-list-modal/camera-list-modal.component.ts
+++ src/app/routes/safety-map/safety-map-list/safety-map-enterprise/camera-list-modal/camera-list-modal.component.ts
@@ -1,23 +1,40 @@
-import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import videojs from 'video.js';
 
 @Component({
   selector: 'camera-list-modal',
   templateUrl: './camera-list-modal.component.html',
   styleUrls: ['./camera-list-modal.component.scss'],
 })
-export class CameraListModalComponent implements OnInit {
+export class CameraListModalComponent implements OnInit, AfterViewInit {
   @Input() showModel: boolean;
   @Output() showModelChange = new EventEmitter<boolean>();
+  player: any;
+
   constructor(private cdr: ChangeDetectorRef) {
     this.showModel = false;
 
 
   }
+
   close() {
+    this.player.dispose();
     this.showModel = false;
     this.showModelChange.emit(false);
   }
+
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    this.player = videojs('my-video', {
+      sources: [{ src: 'http://cyberplayerplay.kaywang.cn/cyberplayer/demo201711-L1.m3u8' }],
+      // sources: [{ src: 'http://192.168.10.9:83/openUrl/618vdN6/live.m3u8'}],
+      loop: true,
+      muted: true,
+      height: 400,
+      controls: true,
+    });
+    this.player.autoplay('muted');
+  }
 }
