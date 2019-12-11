4a0b9ffd2376ca064eafb5de8e459d32627e69da
huajian <287643967@qq.com>
2019-12-11 16:54:34 +0800

    实时报警添加websocket

--- src/app/routes/safety-map/safety-map-list/safety-map-enterprise/alarm-list/alarm-list.component.ts
+++ src/app/routes/safety-map/safety-map-list/safety-map-enterprise/alarm-list/alarm-list.component.ts
@@ -1,18 +1,20 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { STColumn, STData } from '@delon/abc';
 import { AlarmListService, AlarmListServiceNs } from '@core/biz-services/alarm-management/alarm-list.service';
 import RealTimeAlarmModel = AlarmListServiceNs.RealTimeAlarmModel;
 import { MapPipe } from '@shared/directives/pipe/map.pipe';
+
 
 @Component({
   selector: 'alarm-list',
   templateUrl: './alarm-list.component.html',
   styleUrls: ['./alarm-list.component.scss'],
 })
-export class AlarmListComponent implements OnInit {
+export class AlarmListComponent implements OnInit,OnDestroy {
   columns: STColumn[];
   dataList: RealTimeAlarmModel[];
   @Input() entprId: number;
+  ws: WebSocket;//定义websocket
 
   constructor(private alarmListService: AlarmListService, private cdr: ChangeDetectorRef) {
     this.dataList = [];
@@ -21,7 +23,12 @@ export class AlarmListComponent implements OnInit {
   private initTable(): void {
     this.columns = [
       { title: '设备编号', index: 'sensorNo', width: 180 },
-      { title: '报警类型', index: 'alarmType', width: 180 ,  format: (item: STData, _col: STColumn, index) => this.format(item[_col.indexKey], _col.indexKey),},
+      {
+        title: '报警类型',
+        index: 'alarmType',
+        width: 180,
+        format: (item: STData, _col: STColumn, index) => this.format(item[_col.indexKey], _col.indexKey),
+      },
       { title: '报警时间', index: 'alarmStartTime', width: 180, type: 'date' },
     ];
   }
@@ -35,9 +42,46 @@ export class AlarmListComponent implements OnInit {
     this.cdr.markForCheck();
   };
 
+  connectWs() {
+    if (this.ws != null) {
+      this.ws.close();
+    }
+    this.ws = new WebSocket('ws://172.16.1.18:8081/websocket');
+    this.ws.onopen = (e) => {
+      //socket 开启后执行，可以向后端传递信息
+      // this.ws.send('sonmething');
+
+    };
+    this.ws.onmessage = (e) => {
+      //socket 获取后端传递到前端的信息
+      // this.ws.send('sonmething');
+      if (e.data !== '连接成功') {
+        const tempArray = JSON.parse(e.data);
+        this.dataList = (tempArray as any[]).filter((item) => {
+          return item.entprId === this.entprId;
+        });
+        this.cdr.markForCheck();
+      }
+    };
+    this.ws.onerror = (e) => {
+      //socket error信息
+      console.log(e);
+
+    };
+    this.ws.onclose = (e) => {
+      //socket 关闭后执行
+      console.log(e);
+    };
+  }
+
   ngOnInit() {
     this.initTable();
     this.getAlarmList();
+    this.connectWs();
+  }
+
+  ngOnDestroy(): void {
+    this.ws.close();
   }
 
 }
