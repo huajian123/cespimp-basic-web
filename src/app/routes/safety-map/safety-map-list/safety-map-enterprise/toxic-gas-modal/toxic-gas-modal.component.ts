20a5c389bec7b928936c3c45389e4022621f3109
huajian <287643967@qq.com>
2019-12-16 15:44:10 +0800

    安全一张图添加实时数据

--- src/app/routes/safety-map/safety-map-list/safety-map-enterprise/toxic-gas-modal/toxic-gas-modal.component.ts
+++ src/app/routes/safety-map/safety-map-list/safety-map-enterprise/toxic-gas-modal/toxic-gas-modal.component.ts
@@ -13,6 +13,7 @@ import SensorInfoWebSocketModel = SafetyMapServiceNs.SensorInfoWebSocketModel;
 import { webSocketIp } from '@env/environment';
 import { MapPipe } from '@shared/directives/pipe/map.pipe';
 import WebSocketTypeEnum = SafetyMapServiceNs.WebSocketTypeEnum;
+import { subDays, addDays } from 'date-fns';
 
 @Component({
   selector: 'toxic-gas-modal',
@@ -24,34 +25,119 @@ import WebSocketTypeEnum = SafetyMapServiceNs.WebSocketTypeEnum;
 })
 export class ToxicGasModalComponent implements OnInit, OnDestroy {
   @Input() id;
-  char: any;
-  Option: any;
+  realTimeChart: any;
+  historyChart: any;
+  dateRange = [subDays(new Date(), 1), new Date()];
+  realTimeOptions: any;
   historyOption: any;
-  value: any;
-  kd: any;
-  /*  data: any;*/
-  borderColor: any;
-  mercuryColor: any;
+  expandForm: boolean;
   @Input() showModel: boolean;
   @Output() showModelChange = new EventEmitter<boolean>();
-  dateRange = [];
   ws: WebSocket;//定义websocket
   currentDataInfo: SensorInfoWebSocketModel;
-  historyLineValue: {
-    time: string[],
-    value: number[]
-  };
-  constructor(private cdr: ChangeDetectorRef,private safetyMapService: SafetyMapService) {
-    this.showModel = false;
-    this.value = 40.0;
-    this.borderColor = '#fd4d49';
-    this.mercuryColor = '#fd4d49';
-    this.kd = [];
-    this.historyLineValue = {
-      time: [],
-      value: [],
-    };
+  /*实时数据相关*/
+  legendData = ['实时压力'];
+  time = 0;
+  zoomStart = 0;
+  zoomEnd = 100;
+  xAxisData = [];
+  seriesData = [
+    {
+      name: '实时压力',
+      type: 'line',
+      lineStyle: {
+        width: 1,
+      },
+      showSymbol: false,
+      smooth: true, //是否平滑显示折现
+      data: [],
+      markLine: { //最大值和最小值
+        data: [
+          {
+            name: '阈值300',
+            yAxis: 20,
+            label: {
+              show: 'true',
+              formatter: '一级阈值',
+            },
+            lineStyle: {
+              normal: {
+                width: 2,
+                color: '#FF5D1D',
+              },
+            },
 
+          },
+          {
+            name: '阈值100',
+            yAxis: 100,
+            label: {
+              show: 'true',
+              formatter: '二级阈值',
+            },
+            lineStyle: {
+              normal: {
+                width: 2,
+                color: '#FF5D1D',
+              },
+            },
+          }],
+        label: {},
+      },
+    },
+  ];
+
+  /*历史数据相关*/
+  historyLegendData = ['历史压力'];
+  historyZoomStart = 0;
+  historyZoomEnd = 100;
+  historyXAxisData = [];
+  historySeriesData = [
+    {
+      name: '历史压力',
+      type: 'line',
+      lineStyle: {
+        width: 1,
+      },
+      showSymbol: false,
+      smooth: true, //是否平滑显示折现
+      data: [],
+      markLine: { //最大值和最小值
+        data: [
+          {
+            name: '阈值300',
+            yAxis: 300,
+            label: {
+              show: 'true',
+            },
+            lineStyle: {
+              normal: {
+                width: 2,
+                color: '#FF5D1D',
+              },
+            },
+
+          },
+          {
+            name: '阈值100',
+            yAxis: 100,
+            label: {
+              show: 'true',
+            },
+            lineStyle: {
+              normal: {
+                width: 2,
+                color: '#FF5D1D',
+              },
+            },
+          }],
+      },
+    },
+  ];
+
+  constructor(private cdr: ChangeDetectorRef, private safetyMapService: SafetyMapService) {
+    this.showModel = false;
+    this.expandForm = false;
     this.currentDataInfo = {
       sensorName: '',
       sensorNo: '',
@@ -69,192 +155,281 @@ export class ToxicGasModalComponent implements OnInit, OnDestroy {
     this.showModelChange.emit(false);
   }
 
-// 因为柱状初始化为0，温度存在负值，所以，原本的0-100，改为0-130，0-30用于表示负值
-  getData(value) {
-    return [value + 30];
-  }
-
-  data = this.getData(40);
-
-  initCeShiOption() {
-    this.Option = {
-      backgroundColor: '#ffffff',
-      color: ['#37A2DA', '#32C5E9', '#67E0E3'],
-      series: [{
-        name: '业务指标',
-        type: 'gauge',
-        detail: {
-          formatter: '{value}%',
+  initChartOption() {
+    this.realTimeOptions = {
+      color: ['#327bfa'],
+      title: {
+        show: false,
+        text: '实时压力',
+        x: '50%',
+        y: '5%',
+        textAlign: 'center',
+        textStyle: {
+          color: '#bac7e5',
+          fontSize: '30',
+          fontWeight: 'normal',
         },
-        axisLine: {
-          show: true,
-          lineStyle: {
-            width: 30,
-            shadowBlur: 0,
-            color: [
-              [0.3, '#67e0e3'],
-              [0.7, '#37a2da'],
-              [1, '#fd666d'],
-            ],
-          },
-        },
-        data: [{
-          value: 36,
-          name: '承压率',
-        }],
 
-      }],
-    };
-
-    this.historyOption = {
-      backgroundColor: '#FFF',
-      grid: {
-        top: '9%',
-        bottom: '19%',
-        left: '6%',
-        right: '4%',
       },
       tooltip: {
         trigger: 'axis',
-        label: {
-          show: true,
+        backgroundColor: '#282d3b',
+        textStyle: {
+          color: '#bac7e5',
         },
+      },
+      legend: {
+        bottom: '0',
+        data: this.legendData,
+        textStyle: {
+          color: '#bac7e5',
+        },
+        icon: 'rect',
+        itemWidth: 12,
+        itemHeight: 12,
+      },
+      grid: {
+        top: '10%',
+        left: '5%',
+        right: '5%',
+        bottom: '35%',
+        containLabel: true,
+      },
+      toolbox: {
+        show: false,
       },
       xAxis: {
-        boundaryGap: true, //默认，坐标轴留白策略
-        axisLine: {
-          show: false,
-        },
-        splitLine: {
-          show: false,
-        },
+        type: 'category',
+        boundaryGap: false,
         axisTick: {
-          show: false,
-          alignWithLabel: true,
-        },
-        data: this.historyLineValue.time,
-      },
-      yAxis: {
-        axisLine: {
-          show: false,
-        },
-        splitLine: {
           show: true,
+        },
+        axisLine: {
           lineStyle: {
-            type: 'dashed',
-            color: 'rgba(33,148,246,0.2)',
+            color: '#7c88a7',
           },
         },
-        axisTick: {
+        splitLine: {
           show: false,
         },
-        splitArea: {
-          show: true,
-          areaStyle: {
-            color: 'rgb(245,250,254)',
+        axisLabel: {
+          color: '#bac7e5',
+        },
+        data: this.xAxisData,
+      },
+      dataZoom: [{
+        type: 'slider',
+        /*类型*/
+        xAxisIndex: 0,
+        /*对应的轴*/
+        bottom: '23%',
+        /*位置，定位*/
+        start: 0,
+        /*开始*/
+        end: 100,
+        /*结束*/
+        handleIcon: 'M0,0 v9.7h5 v-9.7h-5 Z',
+        handleStyle: {
+          /*手柄的样式*/
+          color: '#00b0ff',
+          borderColor: '#00b0ff',
+        },
+        backgroundColor: '#233239',
+        borderColor: '#233239',
+        /*背景 */
+        dataBackground: {
+          /*数据背景*/
+          lineStyle: {
+            color: '#000000',
           },
+          areaStyle: {
+            color: '#ddd',
+          },
+        },
+        fillerColor: 'rgba(31,178,251,0.2)',
+        /*被start和end遮住的背景*/
+        labelFormatter: function(value, params) {
+          /*拖动时两端的文字提示*/
+          var str = '';
+          if (params.length > 5) {
+            str = params.substring(0, 5) + '…';
+          } else {
+            str = params;
+          }
+          return str;
+        },
+        textStyle: {
+          color: '#bac7e5',
+        },
+      }],
+      yAxis: {
+        type: 'value',
+        name: '℃',
+        nameTextStyle: {
+          color: '#bac7e5',
+        },
+        max: 120,
+        axisTick: {
+          show: true,
+        },
+        splitLine: {
+          show: false,
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#7c88a7',
+          },
+        },
+        axisLabel: {
+          color: '#bac7e5',
         },
       },
-      series: [{
-        type: 'line',
-        symbol: 'circle',
-        markLine: { //最大值和最小值
-          data: [
-            {
-              name: '阈值300',
-              yAxis: 300,
-              label: {
-                show: 'true',
-              },
-              lineStyle: {
-                normal: {
-                  width: 2,
-                  color: '#FF5D1D',
-                },
-              },
+      backgroundColor: '#233239',
+      series: this.seriesData,
+    };
+    this.initHistoryOption();
+  }
 
-            },
-            {
-              name: '阈值100',
-              yAxis: 100,
-              label: {
-                show: 'true',
-              },
-              lineStyle: {
-                normal: {
-                  width: 2,
-                  color: '#FF5D1D',
-                },
-              },
-            }],
+  initHistoryOption() {
+    this.historyOption = {
+      color: ['#327bfa'],
+      title: {
+        show: false,
+        text: '实时压力',
+        x: '50%',
+        y: '5%',
+        textAlign: 'center',
+        textStyle: {
+          color: '#bac7e5',
+          fontSize: '30',
+          fontWeight: 'normal',
         },
-        symbolSize: 7,
-        lineStyle: {
-          color: 'rgb(33,148,246)',
-          shadowBlur: 12,
-          shadowColor: 'rgb(33,148,246,0.9)',
-          shadowOffsetX: 1,
-          shadowOffsetY: 1,
-        },
-        itemStyle: {
-          color: 'rgb(33,148,246)',
-          borderWidth: 1,
-          borderColor: '#FFF',
-        },
-        label: {
-          show: false,
-          distance: 1,
-          emphasis: {
-            show: true,
-            offset: [25, -2],
-            //borderWidth:1,
-            // borderColor:'rgb(33,148,246)',
-            //formatter:'{bg|{b}\n数据量:{c}}',
-            backgroundColor: {
-              image: '../../../../../../assets/66.png',
-            },
-            color: '#FFF',
-            padding: [8, 20, 8, 6],
-            //width:60,
-            height: 36,
-            formatter: function(params) {
-              let name = params.name;
-              let value = params.data;
-              let str = name + '\n数据量：' + value;
-              return str;
-            },
-            rich: {
-              bg: {
-                backgroundColor: {
-                  image: '../../../../../../assets/66.png',
-                },
-                width: 78,
-                //height:42,
-                color: '#FFF',
-                padding: [20, 0, 20, 10],
-              },
-              br: {
-                width: '100%',
-                height: '100%',
-              },
 
-            },
+      },
+      tooltip: {
+        trigger: 'axis',
+        backgroundColor: '#282d3b',
+        textStyle: {
+          color: '#bac7e5',
+        },
+      },
+      legend: {
+        bottom: '0',
+        data: this.historyLegendData,
+        textStyle: {
+          color: '#bac7e5',
+        },
+        icon: 'rect',
+        itemWidth: 12,
+        itemHeight: 12,
+      },
+      grid: {
+        top: '10%',
+        left: '5%',
+        right: '5%',
+        bottom: '35%',
+        containLabel: true,
+      },
+      toolbox: {
+        show: false,
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        axisTick: {
+          show: true,
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#7c88a7',
           },
         },
-        data: this.historyLineValue.value,
+        splitLine: {
+          show: false,
+        },
+        axisLabel: {
+          color: '#bac7e5',
+        },
+        data: this.historyXAxisData,
+      },
+      dataZoom: [{
+        type: 'slider',
+        /*类型*/
+        xAxisIndex: 0,
+        /*对应的轴*/
+        bottom: '23%',
+        /*位置，定位*/
+        start: 0,
+        /*开始*/
+        end: 100,
+        /*结束*/
+        handleIcon: 'M0,0 v9.7h5 v-9.7h-5 Z',
+        handleStyle: {
+          /*手柄的样式*/
+          color: '#00b0ff',
+          borderColor: '#00b0ff',
+        },
+        backgroundColor: '#233239',
+        borderColor: '#233239',
+        /*背景 */
+        dataBackground: {
+          /*数据背景*/
+          lineStyle: {
+            color: '#000000',
+          },
+          areaStyle: {
+            color: '#ddd',
+          },
+        },
+        fillerColor: 'rgba(31,178,251,0.2)',
+        /*被start和end遮住的背景*/
+        labelFormatter: function(value, params) {
+          /*拖动时两端的文字提示*/
+          var str = '';
+          if (params.length > 5) {
+            str = params.substring(0, 5) + '…';
+          } else {
+            str = params;
+          }
+          return str;
+        },
+        textStyle: {
+          color: '#bac7e5',
+        },
       }],
+      yAxis: {
+        type: 'value',
+        name: '℃',
+        nameTextStyle: {
+          color: '#bac7e5',
+        },
+        max: 120,
+        axisTick: {
+          show: true,
+        },
+        splitLine: {
+          show: false,
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#7c88a7',
+          },
+        },
+        axisLabel: {
+          color: '#bac7e5',
+        },
+      },
+      backgroundColor: '#233239',
+      series: this.historySeriesData,
     };
   }
 
-  onChartInit(chart: any) {
-    this.char = chart;
-    setTimeout(() => {
-      this.char.resize();
-    }, 400);
+  onRealTimeChartInit(chart: any) {
+    this.realTimeChart = chart;
   }
 
-  ngOnInit() {
-    this.initCeShiOption();
+  onHistoryChartInit(chart: any) {
+    this.historyChart = chart;
   }
 
   connectWs() {
@@ -263,54 +438,123 @@ export class ToxicGasModalComponent implements OnInit, OnDestroy {
     }
     this.ws = new WebSocket(`ws://${webSocketIp}:8081/websocket/${WebSocketTypeEnum.PoisonGas}`);
     this.ws.onopen = (e) => {
-      console.log(e);
       //socket 开启后执行，可以向后端传递信息
       // this.ws.send('sonmething');
-
     };
     this.ws.onmessage = (e) => {
       //socket 获取后端传递到前端的信息
       // this.ws.send('sonmething');
       if (e.data !== '-连接已建立-') {
         const tempArray = JSON.parse(e.data);
-        this.currentDataInfo = tempArray.filter(({id}) => {
+        this.currentDataInfo = tempArray.filter(({ id }) => {
           return id === this.id;
         })[0];
-        console.log(this.currentDataInfo);
-        this.historyLineValue.value = [];
-        this.historyLineValue.time = [];
-        this.currentDataInfo.historyData.forEach(({ reportTime, sensorValue }) => {
-          this.historyLineValue.time.push(new MapPipe().transform(reportTime, 'date:HH:mm:ss'));
-          this.historyLineValue.value.push(sensorValue)
+
+        this.setPercent(this.currentDataInfo.currentValue, {
+          first: this.currentDataInfo.firstAlarmThreshold,
+          second: this.currentDataInfo.secondAlarmThreshold,
         });
-        this.initCeShiOption();
-        this.char.resize();
-        console.log(this.historyLineValue);
         this.cdr.markForCheck();
       }
     };
     this.ws.onerror = (e) => {
       //socket error信息
-      console.log(e);
-
     };
     this.ws.onclose = (e) => {
       //socket 关闭后执行
-      console.log(e);
     };
   }
 
   async openWebSocketFn() {
-    await this.safetyMapService.openWebsocket();
     this.connectWs();
+    await this.safetyMapService.openWebsocket();
+  }
+
+  /*实时数据*/
+  timef() {
+    let str;
+    this.time += 3;
+    let m = Math.round(this.time / 60);
+    str = m < 10 ? '0' + m : m;
+    str += ':';
+    let s = this.time % 60;
+    str += s < 10 ? '0' + s : s;
+    return str;
+  }
+
+  // 实时数据塞值
+  setPercent(p, alarmThresold) {
+    this.realTimeOptions.xAxis.data.push(this.timef());
+    this.realTimeOptions.series[0].data.push(p);
+    this.realTimeOptions.dataZoom[0].start = this.zoomStart;
+    this.realTimeOptions.dataZoom[0].end = this.zoomEnd;
+    this.seriesData[0].markLine.data[0].yAxis = alarmThresold.first;
+    this.seriesData[0].markLine.data[1].yAxis = alarmThresold.second;
+    this.realTimeChart.setOption(this.realTimeOptions);
+  }
+
+  // 历史数据塞值
+  historySetPercent(p, t) {
+    this.historyOption.xAxis.data.push(t);
+    this.historyOption.series[0].data.push(p);
+    this.historyOption.dataZoom[0].start = this.historyZoomStart;
+    this.historyOption.dataZoom[0].end = this.historyZoomEnd;
+  }
+
+  // 获取历史数据
+  async getHistoryData() {
+    const params = {
+      id: this.id, beginTime: this.dateRange[0], endTime: this.dateRange[1],
+    };
+    this.historyOption.xAxis.data = [];
+    this.historyOption.series[0].data = [];
+    const data = await this.safetyMapService.getSensorHistory(params);
+    data.forEach(({ reportTime, sensorValue }) => {
+      this.historySetPercent(sensorValue, new MapPipe().transform(reportTime, 'date:MM-dd HH:mm:ss'));
+    });
+    this.historyChart.setOption(this.historyOption);
+  }
+
+  dataZoomChange(event) {
+    this.zoomStart = event.start;
+    this.zoomEnd = event.end;
+  }
+
+  disabledDate(current: Date) {
+    return current.getTime()<this.dateRange[0].getTime()||current.getTime() >= (addDays(this.dateRange[0], 4).getTime());
+  }
+
+  // 选择历史数据tab
+  selHistoryTab() {
+    this.getHistoryData();
+  }
+
+  // 历史数据改变第一个搜索条件
+  changeStartSearchDate(e) {
+    this.dateRange[1] = null;
+  }
+
+  // 选择时间
+  searchHistroyByTimeRange() {
+    this.getHistoryData();
+  }
+
+  resetSearch() {
+    this.historyOption.xAxis.data = [];
+    this.historyOption.series[0].data = [];
+    this.dateRange = [subDays(new Date(), 1), new Date()];
+    this.getHistoryData();
+  }
+
+  ngOnInit() {
+    this.openWebSocketFn();
+  }
+
+  ngOnDestroy(): void {
+    this.ws.close();
   }
 
   ngAfterViewInit(): void {
-    this.initCeShiOption();
-    this.openWebSocketFn();
-    this.cdr.markForCheck();
-  }
-  ngOnDestroy(): void {
-    this.ws.close();
+    this.initChartOption();
   }
 }
