d9d67742c7a195e5c40df1d7952a73a252c7a142
huajian <287643967@qq.com>
2020-01-20 10:36:38 +0800

    添加操作成功统一返回

--- src/app/core/net/http-util.service.ts
+++ src/app/core/net/http-util.service.ts
@@ -5,6 +5,7 @@ import { filter, map } from 'rxjs/operators';
 import { _HttpClient } from '@delon/theme';
 import { ActionResult } from '@core/vo/comm/ActionResult';
 import { environment, getwayKey } from '@env/environment';
+import { MessageType, ShowMessageService } from '../../widget/show-message/show-message';
 
 
 export namespace HttpUtilNs {
@@ -41,36 +42,46 @@ export namespace HttpUtilNs {
 
   export class HttpUtilServiceClass {
 
-    constructor(private http: _HttpClient) {
+    constructor(private http: _HttpClient, private messageService: ShowMessageService) {
 
     }
 
     public post<T>(path: string, params?, config?: UfastHttpConfig, body?: any): Observable<any> {
       config = config || {};
       const url = this.getFullUrl(config.gateway, path);
-      return this.http.post(url, params, body).pipe(map(item => (item as ActionResult<T>).data));
+      return this.http.post(url, params, body).pipe(filter((item) => this.handleFilter(item, config.needSuccessInfo)), map(item => (item as ActionResult<T>).data));
     }
+
+    handleFilter(item, needSuccessInfo) {
+      if (item.code !== 0) {
+        this.messageService.showAlertMessage('', item.msg, MessageType.Warning);
+      } else if (needSuccessInfo) {
+        this.messageService.showToastMessage('操作成功', MessageType.Success);
+      }
+      return item.code === 0;
+    }
+
 
     public get<T>(path: string, params?, config?: UfastHttpConfig): Observable<any> {
       config = config || {};
       const url = this.getFullUrl(config.gateway, path);
-      return this.http.get(url, params, config).pipe(map(item => (item as ActionResult<T>).data));
+      return this.http.get(url, params, config).pipe(filter((item) => this.handleFilter(item, config.needSuccessInfo)), map(item => (item as ActionResult<T>).data));
     }
 
     public put<T>(path: string, params?, config?: UfastHttpConfig): Observable<any> {
       config = config || {};
       const url = this.getFullUrl(config.gateway, path);
-      return this.http.put(url, params, config).pipe(map(item => (item as ActionResult<T>).data));
+      return this.http.put(url, params, config).pipe(filter((item) => this.handleFilter(item, config.needSuccessInfo)), map(item => (item as ActionResult<T>).data));
     }
 
     public del<T>(path: string, params?, config?: UfastHttpConfig): Observable<any> {
       config = config || {};
       const url = this.getFullUrl(config.gateway, path);
-      return this.http.delete(url, params, config).pipe(map(item => (item as ActionResult<T>).data));
+      return this.http.delete(url, params, config).pipe(filter((item) => this.handleFilter(item, config.needSuccessInfo)), map(item => (item as ActionResult<T>).data));
     }
 
     public getFullUrl(baseUrlName: string, path: string, isUpload?: boolean): string {
-      const url = environment.baseUrl[baseUrlName || getwayKey.Bs] + path+'?_allow_anonymous=true';
+      const url = environment.baseUrl[baseUrlName || getwayKey.Bs] + path + '?_allow_anonymous=true';
       return url;
     }
   }
@@ -80,8 +91,8 @@ export namespace HttpUtilNs {
   providedIn: 'root',
 })
 export class HttpUtilService extends HttpUtilNs.HttpUtilServiceClass {
-  constructor(http: _HttpClient) {
-    super(http);
+  constructor(http: _HttpClient, messageService: ShowMessageService) {
+    super(http, messageService);
   }
 }
 
